09bc09874c82328bf0f59cefa1d70854ed78c912
rahirim93 <59837269+rahirim93@users.noreply.github.com>
2022-11-21 21:57:14 +0300

    Подправлен интерфейс и стиль

--- TraningTimer/app/src/main/java/com/example/traningtimer/MainActivity.kt
+++ TraningTimer/app/src/main/java/com/example/traningtimer/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.traningtimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
